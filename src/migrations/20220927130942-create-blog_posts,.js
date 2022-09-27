d79b6c4ed97ebce2d86a1e5f9c77626bd8962a64
allanzaparoli <allanzaparoli@gmail.com>
2022-09-27 17:16:23 -0300

    requisito 1

---
+++ src/migrations/20220927130942-create-blog_posts,.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('blog_posts', {
+
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+
+      title: {
+        type: Sequelize.STRING
+      },
+
+      content: {
+        type: Sequelize.STRING
+      },
+
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        references: {
+          model: 'users',
+          key: 'id'
+        }
+      },
+
+      published: {
+        type: Sequelize.DATE
+      },
+
+      updated: {
+        type: Sequelize.DATE
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('blog_posts');
+  }
+};
